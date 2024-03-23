1234830f2c01e40fb234ca0c0cd298fc01e2daff
Tom Mossington <tmossington@gmail.com>
2024-03-23 19:33:24 -0400

    Console will now report the members present in the server for further verification.

--- bot.py
+++ bot.py
@@ -19,7 +19,8 @@ async def on_ready():
         f'{client.user} has connected to Discord'
         f'{guild.name}(id: {guild.id})'
     )
-    
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
     
 
 client.run(TOKEN)
