cde8addfc3c241a93a011ab4288fa8b5181f7c00
Tom Mossington <tmossington@gmail.com>
2024-03-23 20:13:59 -0400

    Added function where PyBot will now respond in Discord server if a user says certain words.

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import os
 import discord
 from dotenv import load_dotenv
+import random
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -21,6 +22,17 @@ async def on_ready():
     )
     members = '\n - '.join([member.name for member in guild.members])
     print(f'Guild Members:\n - {members}')
-    
 
+@client.event
+async def on_message(message):
+    username = str(message.author).split("#")[0]
+    channel = str(message.channel.name)
+    user_message = str(message.content)
+
+    if message.author == client.user:
+        return
+    
+    if user_message == "Hello":
+        await message.channel.send(f"Hello {username}")
+        return
 client.run(TOKEN)
